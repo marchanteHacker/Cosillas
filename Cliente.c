85d0ffd92569b39f39ea9c98085e9fd6e3a087ea
José Luis Leal García <103925590+joselulg03@users.noreply.github.com>
2023-11-02 19:25:17 +0100

    Cliente-Servidor Suma Array Paralelo

--- Cliente.c
+++ Cliente.c
@@ -13,7 +13,7 @@
 int main(int argc, char *argv[])
 {
     // Valor inicial del vector A, valor inicial del vector B.
-    float iniA, iniB;
+    int iniA, iniB;
 
     // Tamaño de los vectores, índice de recorrido.
     int tam, i;
@@ -29,21 +29,96 @@ int main(int argc, char *argv[])
 
     // **0.5 puntos** EJ 1.1. Tratamiento de errores en los parámetros de entrada.
 
+    if(argc < 1){
+        printf("Comando incorrecto -> ./Cliente tam");
+        exit(EXIT_FAILURE);
+    }
+
     // **0.5 puntos** EJ 1.2. Inicialización de las variables iniA, iniB, y tam.
+
+    tam = atoi(argv[1]);
+    int vectorA[tam], vectorB[tam];
+
+    for(int i = 0; i < tam; i++){
+        printf("Introduce el valor para la posicion %d del vector A: ", i);
+        scanf("%d", &iniA);
+        vectorA[i] = iniA;
+        printf("Introduce el valor para la posicion %d del vector B: ", i);
+        scanf("%d", &iniB);
+        vectorB[i] = iniB;
+    }
+
+    printf("Vector A ->");
+    for(int i = 0; i < tam; i++){
+        printf("%d ",vectorA[i]);
+    }
+
+    printf("\n");
+
+    printf("Vector B ->");
+    for(int i = 0; i < tam; i++){
+        printf("%d ",vectorB[i]);
+    }
 
     // **0.5 puntos** EJ 1.4. Construcción del socket servidor y tratamiento del error.
 
+    socketServidor = socket(AF_INET, SOCK_STREAM, 0);
+    if(socketServidor == -1){
+        perror("SOCKET");
+        exit(EXIT_FAILURE);
+    }
+
     // **0.5 puntos** EJ 1.5. Construcción de la estructura de dirección del servidor y tratamiento del error.
+
+    direccionSocketServidor.sin_family = AF_INET;
+    direccionSocketServidor.sin_port = htons(PUERTOSERVIDOR);
+    if(inet_pton(AF_INET, IPSERVIDOR, &direccionSocketServidor.sin_addr.s_addr) == -1){
+        perror("INET_PTON");
+        close(socketServidor);
+        exit(EXIT_FAILURE);
+    }
 
     // **0.5 puntos** EJ 1.6. Realizar conexión con el servidor y tratamiento del error.
 
+    if(connect(socketServidor, (struct sockaddr *)&direccionSocketServidor, sizeof(direccionSocketServidor)) == -1){
+        perror("CONNECT");
+        close(socketServidor);
+        exit(EXIT_FAILURE);
+    }
+
     // **0.5 puntos** EJ 1.7. Envío del tamaño de los vectores y tratamiento del error.
+
+    if(write(socketServidor, &tam, sizeof(tam)) == -1){
+        perror("WRITE");
+        close(socketServidor);
+        exit(EXIT_FAILURE);
+    }
 
     // **0.5 puntos** EJ 1.8. Envío del vector A al servidor y tratamiento del error.
 
+    if(write(socketServidor, vectorA, sizeof(vectorA)) == -1){
+        perror("WRITE");
+        close(socketServidor);
+        exit(EXIT_FAILURE);
+    }
+
     // **0.5 puntos** EJ 1.9. Envío del vector B al servidor y tratamiento del error.
 
+    if(write(socketServidor, vectorB, sizeof(vectorB)) == -1){
+        perror("WRITE");
+        close(socketServidor);
+        exit(EXIT_FAILURE);
+    }
+
     // **0.5 puntos** EJ 1.10. Recepción de los resultados del servidor y tratamiento del error.
+
+    if(read(socketServidor, resultado, sizeof(resultado)) == -1){
+        perror("READ");
+        close(socketServidor);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("\n%s\n", resultado);
 
     // Cierre del socket servidor.
     close(socketServidor);
--- Servidor.c
+++ Servidor.c
@@ -12,73 +12,135 @@
 
 int main(void)
 {
-	
-   	// Descriptor del socket del servidor, descriptor del socket del cliente y tamaño de la estructura de la dirección del cliente.
-	int socketServidor,socketCliente,tamanyoDireccionCliente;
 
-	// Estructura de dirección del servidor y del cliente.
-    struct sockaddr_in direccionSocketServidor,direccionSocketCliente; 
-    
+    // Descriptor del socket del servidor, descriptor del socket del cliente y tamaño de la estructura de la dirección del cliente.
+    int socketServidor, socketCliente, tamanyoDireccionCliente;
+
+    // Estructura de dirección del servidor y del cliente.
+    struct sockaddr_in direccionSocketServidor, direccionSocketCliente;
+
     // Resultado a enviar al cliente.
     char resultado[SIZE];
 
-	// Tamaño del vector recibido, índice de recorrido.
-	int tam, i;
+    // Tamaño del vector recibido, índice de recorrido.
+    int tam, i;
 
-	// Resultado numérico de la operación solicitada por el cliente.
-	float r;
+    // Resultado numérico de la operación solicitada por el cliente.
+    // int r;
 
-	// **0.5 puntos** EJ 2.1. Construcción del socket servidor y tratamiento del error.
+    // **0.5 puntos** EJ 2.1. Construcción del socket servidor y tratamiento del error.
 
-  
+    socketServidor = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketServidor == -1)
+    {
+        perror("SOCKET");
+        exit(EXIT_FAILURE);
+    }
 
-	// **0.5 puntos** EJ 2.2. Construcción de la estructura de dirección del servidor.
-  	
+    // **0.5 puntos** EJ 2.2. Construcción de la estructura de dirección del servidor.
+
+    direccionSocketServidor.sin_family = AF_INET;
+    direccionSocketServidor.sin_port = htons(PUERTOSERVIDOR);
+    direccionSocketServidor.sin_addr.s_addr = INADDR_ANY;
 
     // **0.5 puntos** EJ 2.3. Realización del bind y tratamiento del error.
- 	
+
+    if (bind(socketServidor, (struct sockaddr *)&direccionSocketServidor, sizeof(direccionSocketServidor)) == -1)
+    {
+        perror("BIND");
+        close(socketServidor);
+        exit(EXIT_FAILURE);
+    }
+
     // **0.5 puntos** EJ 2.4. Realización del listen y tratamiento del error.
- 	
+
+    if (listen(socketServidor, NUMEROCLIENTESENCOLA) == -1)
+    {
+        perror("LISTEN");
+        close(socketServidor);
+        exit(EXIT_FAILURE);
+    }
 
     // Bucle de espera activa.
-    while(1)
+    while (1)
     {
         printf("[SERVIDOR] Esperando datos\n");
-        
+
         // **0.5 puntos** EJ 2.5. Realización del accept y tratamiento del error.
-  	
 
-        // **0.5 puntos** EJ 2.6. Recepción del tamaño de los vectores A y B enviado por el cliente y tratamiento del error.	   
+        socketCliente = accept(socketServidor, (struct sockaddr *)&direccionSocketCliente, &tamanyoDireccionCliente);
+        if (socketCliente == -1)
+        {
+            perror("SOCKET CLIENTE");
+            exit(EXIT_FAILURE);
+        }
+
+        // **0.5 puntos** EJ 2.6. Recepción del tamaño de los vectores A y B enviado por el cliente y tratamiento del error.
         // **NOTA: en caso de error, cierre sólo la conexión con el socket cliente.
-	
 
-	   
+        if (read(socketCliente, &tam, sizeof(tam)) == -1)
+        {
+            perror("READ");
+            close(socketCliente);
+            exit(EXIT_FAILURE);
+        }
 
-        // **0.5 puntos** EJ 2.7. Recepción del vector A enviado por el cliente y tratamiento del error.	   
+        int vectorA[tam], vectorB[tam];
+
+        // **0.5 puntos** EJ 2.7. Recepción del vector A enviado por el cliente y tratamiento del error.
         // **NOTA: en caso de error, cierre sólo la conexión con el socket cliente.
- 		
 
-        // **0.5 puntos** EJ 2.8. Recepción del vector B enviado por el cliente y tratamiento del error.	   
+        if (read(socketCliente, vectorA, sizeof(vectorA)) == -1)
+        {
+            perror("READ");
+            close(socketCliente);
+            exit(EXIT_FAILURE);
+        }
+
+        // **0.5 puntos** EJ 2.8. Recepción del vector B enviado por el cliente y tratamiento del error.
         // **NOTA: en caso de error, cierre sólo la conexión con el socket cliente.
- 	
 
-	    
+        if (read(socketCliente, vectorB, sizeof(vectorB)) == -1)
+        {
+            perror("READ");
+            close(socketCliente);
+            exit(EXIT_FAILURE);
+        }
 
-		// **0.5 puntos** EJ 2.9. Cálculo del producto escalar de dos vectores en paralelo con planificación dinamica, tamaño de chunk 3 y 2 threads.
-	
-		
+        // **0.5 puntos** EJ 2.9. Cálculo de la Suma de elementos de 2 arreglos en paralelo
 
-        // **0.5 puntos** EJ 2.10. Envío del resultado al cliente y tartamiento del error.	   
+        int r[tam];
+
+	#pragma omp parallel for schedule(static, 1)
+	    for (int i = 0; i < tam; i++)
+		r[i] = vectorA[i] + vectorB[i];
+        
+
+        sprintf(resultado, "Resultado: ");
+        for (int i = 0; i < tam; i++)
+        {
+            sprintf(resultado + strlen(resultado), "%d", r[i]);
+            if (i < tam - 1)
+            {
+                sprintf(resultado + strlen(resultado), ", ");
+            }
+        }
+
+        // **0.5 puntos** EJ 2.10. Envío del resultado al cliente y tartamiento del error.
         // **NOTA: en caso de error, cierre sólo la conexión con el socket cliente.
-  	
-  
-    
-		// Cierre del socket cliente.
+
+        if(write(socketCliente, resultado, sizeof(resultado)) == -1){
+            perror("WRITE");
+            close(socketCliente);
+            exit(EXIT_FAILURE);
+        }
+
+        // Cierre del socket cliente.
         close(socketCliente);
     }
 
-	// Cierre del socket servidor.
+    // Cierre del socket servidor.
     close(socketServidor);
-   
+
     return EXIT_SUCCESS;
 }
